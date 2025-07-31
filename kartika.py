ccb2ceca8fb41e78c0efdff4cb2c5f845c4a1662
gxfarx <giffarhibrahim@gmail.com>
2025-07-31 22:23:06 +0800

    Add files via upload

---
+++ kartika.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+st.set_page_config(page_title="🎉 Selamat Ulang Tahun!", page_icon="🎂", layout="centered")
+
+st.balloons()
+
+st.markdown("""
+    <div style='text-align: center; font-size: 40px; color: #FF69B4; font-weight: bold;'>
+        🎂 Selamat Ulang Tahun, KARTIKAAAAAAA! 🎉
+    </div>
+    """, unsafe_allow_html=True)
+
+st.image("https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif", width=300)
+
+st.markdown("""
+    <div style='background-color: #FFF0F5; padding: 20px; border-radius: 20px; border: 2px dashed #FFB6C1;'>
+        <p style='font-size: 18px; color: #C71585;'>
+            Semoga panjang umur, sehat selaLUUUUUUUUUUU AMINNNNNNNNNN SEMOGAAAAAAAA KITA TETAP TEMENAN SELAMA LAMANYAAAAAAAAA SEMOGA KAMU TAMBAHHHHHHH SYANTIK................... KARTIIIIII OMG SELAMAT ULANG TAHUN TERIMAKASIH SUDAH HODUP DAN SUDAH MAU MEMBAGI WAKTU UNTUK AKU MAKASIHHHHHHHHH KARRRRRRRRRRRRRRRRRR<br><br>
+            Hari ini spesial banget, dan kamu juga spesial EAAAAAAAAAAAAAAAAAA 💖<br><br>
+            Nikmati hari ulang tahunmu dengan senyum paling manis HAHAHAHAHA! 🎈🎁
+        </p>
+        <p style='text-align: right; font-size: 16px;'>- Dari sahabat rahasia 🌸</p>
+    </div>
+    """, unsafe_allow_html=True)
+
+st.markdown("---")
+st.caption("✨ Dibuat dengan cinta oleh giparrrrrrrr 💌")
